a5339ef401ba3a46d2e91a487dc1deef27ec37c4
d-mali <daumantasm.malinauskas@gmail.com>
2023-11-30 23:00:37 +0200

    Fixed

--- CarDealership.js
+++ CarDealership.js
@@ -26,25 +26,28 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+//nav onclick
 function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
 
-    // Get all elements with class="tabcontent" and hide them
+    // hide all tabcontent
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
 
-    // Get all elements with class="tablinks" and remove the class "active"
+    // remove nav active
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
+        tablinks[i].classList.remove("active");
     }
 
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
+    // show active tav and set active
+    document.getElementById(tabName).style.display = "flex";
+    evt.currentTarget.classList.add("active");
 }
+
+
 
 document.getElementById('openPopup').addEventListener('click', function () {
     var form = document.getElementById('popupForm');
